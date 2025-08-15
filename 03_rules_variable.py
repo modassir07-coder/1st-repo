2a2faef696e2a2ea3e704685f6243a0c067cf48c
modassir07-coder <mmodassir478@gmail.com>
2025-08-15 23:30:40 +0530

    Add files via upload
    my 1st python project

---
+++ 03_rules_variable.py
@@ -0,0 +1,12 @@
+a= 45
+
+nhenfhv= 757
+
+modassir= 7
+
+_modassir= 45
+
+# @modassir = 45  #invalid due to @ symbol
+# mod@ssir= # invalid due to @ symbol
+
+# Don't delete my project and files (*MMGBL MODASSIR*)
